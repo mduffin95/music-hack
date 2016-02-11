d6b0754b8bb3dbaee2a558d2fabfd3c0df39247d
Gavin Parker <gp14958@my.bristol.ac.uk>
2016-02-11 19:16:23 +0000

    added some stuff

--- client.js
+++ client.js
@@ -1,2 +1,19 @@
 //client js
 console.log("test");
+var conductor = new BandJS();
+conductor.setTimeSignature(4,4);
+conductor.setTempo(120);
+var piano = conductor.createInstrument();
+conductor.setOnFinishedCallback(callback);
+
+document.addEventListener('keydown', function(event) {
+    switch(event.keyCode){
+      case 67:
+      console.log("C");
+      piano.note('quarter', 'C4');
+      piano.play();
+      break;
+
+
+    }
+});
