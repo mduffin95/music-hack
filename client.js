675fb70d35b0b6473ca388925d43b4310ff523ea
dhruv chauhan <dc14690@my.bristol.ac.uk>
2016-02-11 21:04:56 +0000

    added links for guitar and bass

--- client.js
+++ client.js
@@ -87,6 +87,16 @@ function loop_player(){
     player.loop(true);
 }
 
+function choose_guitar(){
+    console.log("guitar chosen");
+    inst = guitar;
+}
+
+function choose_bass(){
+    console.log("bass chosen");
+    inst = bass;
+}
+
 document.addEventListener('keyup', function(event) {
   switch(event.keyCode){
     case 90:
